cf5949875178527cf7f8ce1cc9fb7c050cbf2afb
Aiclys <145050105+Aiclys@users.noreply.github.com>
2024-02-18 16:54:18 +0100

    Add files via upload

--- qtile_install.sh
+++ qtile_install.sh
@@ -1,19 +1,26 @@
 #!/usr/bin/env sh
 
 # Welcome
-echo "Welcome to my Qtile installation script. This script only works on arch based distros"
+echo "Welcome to my Qtile installation script. This script only works on Arch based distros"
+
+# Deciding which branch you wnat to use
+echo "Which theme do you want? You can see a preview of the different themes by taking a look at the different branches of my repo"
+echo "1) All themes"
+echo "2) Colorful"
+echo "3) Gruvbox"
+echo "4) Blue"
+read THEME
 
 # Installing extra packages
 echo "Installing the necessary packages..."
-sudo pacman -S nitrogen picom qtile rofi neofetch kitty neovim
+sudo pacman -S nitrogen picom qtile rofi neofetch kitty neovim emacs git 
 
 echo "What AUR helper do you have installed?"
-echo "1. yay"
-echo "2. paru"
+echo "1) yay"
+echo "2) paru"
 read AUR
 
-if [[ $AUR == '1' ]]
-then
+if [[ $AUR == '1' ]]; then
     echo "Installing qtile-extras with yay..."
     yay -S qtile-extras
 else
@@ -23,12 +30,22 @@ fi
 
 # Cloning the repo
 echo "Cloning the repo..."
-git clone https://github.com/Aiclys/qtile-dotfiles ~/dots
+if [[ $THEME == '1' ]]; then
+   	git clone -b gruvbox https://github.com/Aiclys/dotfiles ~/dots/gruvbox/
+   	git clone -b colorful https://github.com/Aiclys/dotfiles ~/dots/colorful/
+	git clone -b blue https://github.com/Aiclys/dotfiles ~/dots/blue
+elif [[ $THEME == '2' ]]; then
+	git clone -b colorful https://github.com/Aiclys/dotfiles ~/dots
+elif [[ $THEME == '3' ]]; then
+	git clone -b gruvbox https://github.com/Aiclys/dotfiles ~/dots
+elif [[ $THEME == '4' ]]; then
+	git clone -b blue https://github.com/Aiclys/dotfiles ~/dots
+fi
 
 # Backups
 echo "Do you want to create a backup of your current qtile configuration?"
-echo "1. Yes"
-echo "2. No"
+echo "1) Yes"
+echo "2) No"
 read BACKUP
 
 if [[ $BACKUP == '1' ]]
@@ -41,5 +58,6 @@ else
 fi
 
 # Moving my dotfiles to your .config folder
+echo "Which part of my dots do you want to move to your .config?"
 echo "Moving dots into the .config folder..."
 mv ~/dots/qtile ~/.config
